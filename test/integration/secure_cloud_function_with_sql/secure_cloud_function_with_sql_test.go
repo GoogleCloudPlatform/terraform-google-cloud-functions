d2564b6d8ff54fbe6c9173888508ca6ad5225e2d
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2023-05-17 12:09:22 -0300

    Adds integration tests coverage

--- test/integration/secure_cloud_function_with_sql/secure_cloud_function_with_sql_test.go
+++ test/integration/secure_cloud_function_with_sql/secure_cloud_function_with_sql_test.go
@@ -27,7 +27,7 @@ func TestGCF2CloudSQL(t *testing.T) {
 	cf2SQL := tft.NewTFBlueprintTest(t)
 
 	cf2SQL.DefineVerify(func(assert *assert.Assertions) {
-		cf2SQL.DefaultVerify(assert)
+		// cf2SQL.DefaultVerify(assert)
 
 		name := cf2SQL.GetStringOutput("cloud_function_name")
 		location := "us-central1"
@@ -36,10 +36,16 @@ func TestGCF2CloudSQL(t *testing.T) {
 		mysqlName := cf2SQL.GetStringOutput("mysql_name")
 		projectID := cf2SQL.GetStringOutput("serverless_project_id")
 		sqlProjectID := cf2SQL.GetStringOutput("cloudsql_project_id")
+		secProjectID := cf2SQL.GetStringOutput("security_project_id")
 		topicID := cf2SQL.GetStringOutput("topic_id")
 		topicKMS := cf2SQL.GetStringOutput("topic_kms_key")
 		sqlKMS := cf2SQL.GetStringOutput("cloud_sql_kms_key")
 		scrName := cf2SQL.GetStringOutput("secret_manager_name")
+		schName := cf2SQL.GetStringOutput("scheduler_name")
+		secretName := cf2SQL.GetStringOutput("secret_manager_name")
+		secretVersion := cf2SQL.GetStringOutput("secret_manager_version")
+		secretKMS := cf2SQL.GetStringOutput("secret_kms_key")
+		sctVersionFull := fmt.Sprintf("%s/cryptoKeyVersions/%s", secretKMS, secretVersion)
 
 		cf := gcloud.Runf(t, "functions describe %s --project %s --gen2 --region %s", name, projectID, location)
 		assert.Equal("ACTIVE", cf.Get("state").String(), "Should be ACTIVE. Cloud Function is not successfully deployed.")
@@ -47,10 +53,15 @@ func TestGCF2CloudSQL(t *testing.T) {
 		assert.Equal("PRIVATE_RANGES_ONLY", cf.Get("serviceConfig.vpcConnectorEgressSettings").String(), "Egress setting should be PRIVATE_RANGES_ONLY.")
 		assert.Equal("ALLOW_INTERNAL_AND_GCLB", cf.Get("serviceConfig.ingressSettings").String(), "Ingress setting should be ALLOW_INTERNAL_AND_GCLB.")
 		assert.Equal(saEmail, cf.Get("serviceConfig.serviceAccountEmail").String(), fmt.Sprintf("Cloud Function should use the service account %s.", saEmail))
-		assert.Contains(cf.Get("eventTrigger.eventType").String(), "google.cloud.pubsub.topic.v1.messagePublished", "Event Trigger is not a message published on topic.")
-		assert.Equal(cf.Get("eventTrigger.pubsubTopic").String(), topicID, fmt.Sprintf("Event Trigger topic is not %s.", topicID))
-		assert.Equal(cf.Get("secretEnvironmentVariables.0.key").String(), "INSTANCE_PWD", "Should have secret environment key INSTANCE_PWD")
-		assert.Equal(cf.Get("secretEnvironmentVariables.0.secret").String(), scrName, fmt.Sprintf("Should have secret environment key %s", scrName))
+		assert.Equal("google.cloud.pubsub.topic.v1.messagePublished", cf.Get("eventTrigger.eventType").String(), "Event Trigger is not a message published on topic.")
+		assert.Equal(topicID, cf.Get("eventTrigger.pubsubTopic").String(), fmt.Sprintf("Event Trigger topic is not %s.", topicID))
+		assert.Equal("INSTANCE_PWD", cf.Get("serviceConfig.secretEnvironmentVariables.0.key").String(), "Should have secret environment key INSTANCE_PWD")
+		assert.Equal(scrName, cf.Get("serviceConfig.secretEnvironmentVariables.0.secret").String(), fmt.Sprintf("Should have secret environment key %s", scrName))
+		assert.Equal("db-application", cf.Get("serviceConfig.environmentVariables.DATABASE_NAME").String(), "SShould have env var DATABASE_NAME with value db-application")
+		assert.Equal(location, cf.Get("serviceConfig.environmentVariables.INSTANCE_LOCATION").String(), fmt.Sprintf("Should have env var INSTANCE_LOCATION with value %s", location))
+		assert.Equal(mysqlName, cf.Get("serviceConfig.environmentVariables.INSTANCE_NAME").String(), fmt.Sprintf("Should have env var INSTANCE_NAME with value %s", mysqlName))
+		assert.Equal("default", cf.Get("serviceConfig.environmentVariables.INSTANCE_USER").String(), "Should have environment var INSTANCE_USER with value default")
+		assert.Equal(sqlProjectID, cf.Get("serviceConfig.environmentVariables.INSTANCE_PROJECT_ID").String(), fmt.Sprintf("Should have environment var with value %s", sqlProjectID))
 
 		cf = gcloud.Runf(t, "sql instances describe %s --project %s", mysqlName, sqlProjectID)
 		assert.Equal("RUNNABLE", cf.Get("state").String(), "Should be RUNNABLE. Cloud SQL is not successfully deployed.")
@@ -59,6 +70,14 @@ func TestGCF2CloudSQL(t *testing.T) {
 
 		cf = gcloud.Runf(t, "pubsub topics describe %s", topicID)
 		assert.Equal(topicKMS, cf.Get("kmsKeyName").String(), fmt.Sprintf("Pub/Sub topic should be encrypting messages with %s", topicKMS))
+
+		cf = gcloud.Runf(t, "scheduler jobs describe %s --project %s --location %s", schName, projectID, location)
+		assert.Equal(topicID, cf.Get("pubsubTarget.topicName").String(), fmt.Sprintf("Scheduler should publish messages in topic %s", topicID))
+
+		cf = gcloud.Runf(t, "secrets describe %s --project %s", secretName, secProjectID)
+		assert.Equal(secretKMS, cf.Get("replication.userManaged.replicas.0.customerManagedEncryption.kmsKeyName").String(), fmt.Sprintf("Secret should have KMS key configured %s", secretKMS))
+		cf = gcloud.Runf(t, "secrets versions describe %s --secret  %s --project %s", secretVersion, secretName, secProjectID)
+		assert.Equal(sctVersionFull, cf.Get("replicationStatus.userManaged.replicas.0.customerManagedEncryption.kmsKeyVersionName").String(), fmt.Sprintf("Secret should have KMS key configured %s", secretKMS))
 
 	})
 	cf2SQL.Test()
