c9d0cd6ac5130faff1a7fe86515e5583ee3374f1
Grant Sorbo <gtsorbo@google.com>
2023-07-28 15:32:00 +0000

    fix: provider requirement

--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "< 5.0"
+      version = ">= 4.48, < 5.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "< 5.0"
+      version = ">= 4.48, < 5.0"
     }
   }
 
