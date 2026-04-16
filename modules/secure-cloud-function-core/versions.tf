7751772a317f50d4ae64d172c2e6f3c2ae4c8b41
Marcos Vinicius Leal da Silva <marcosvl@ciandt.com>
2026-04-16 15:56:04 -0300

    fix: secure-cloud-function-core to use google v7

--- modules/secure-cloud-function-core/versions.tf
+++ modules/secure-cloud-function-core/versions.tf
@@ -20,11 +20,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.74, < 7.0"
+      version = ">= 4.74, < 8.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "< 7.0"
+      version = "< 8.0"
     }
   }
   provider_meta "google" {
