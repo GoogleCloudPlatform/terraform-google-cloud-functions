bfbfe0e0ed928128cb136184da8575aafffac805
Marcos Vinicius Leal da Silva <marcosvl@ciandt.com>
2026-04-17 17:06:55 -0300

    fix: update runtime to go124 since go121 is not supported

--- examples/secure_cloud_function_internal_server/main.tf
+++ examples/secure_cloud_function_internal_server/main.tf
@@ -269,7 +269,7 @@ module "secure_cloud_function" {
       attribute_value = module.cloudfunction_source_bucket.name
     }]
   }
-  runtime     = "go121"
+  runtime     = "go124"
   entry_point = "helloHTTP"
 
   depends_on = [
